679347d75783962ec1ed786692443bb0b2f18339
SHUVO HALDER <samshuvo00@gmail.com>
2025-01-09 02:32:15 +0600

    adding class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Notification\WelcomeNotification;
+use Illuminate\Support\Facades\Notification;
 
 class UserController extends Controller
 {
